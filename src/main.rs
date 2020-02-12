3c5b8c6091f0b3c13f590e8fa649f8d28b43f555
yukimakura <yukimakura86@gmail.com>
2020-02-12 14:20:30 +0900

    modify ros param

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,28 @@ mod f_msg {
     rosrust::rosmsg_include!(std_msgs/Float64);
 }
 
+struct PARAMETERS{
+    low_point_delta_threshold : f64,
+    low_point_delta_range : f64,
+    x_axis_width_range : f64,
+    detect_range : f64,
+}
+
+fn get_parameters(p : &mut PARAMETERS){
+    p.low_point_delta_threshold = rosrust::param("~low_point_delta_threshold").unwrap().get::<f64>().unwrap_or(0.20);
+    p.low_point_delta_range = rosrust::param("~low_point_delta_range").unwrap().get::<f64>().unwrap_or(0.10);
+    p.x_axis_width_range = rosrust::param("~x_axis_width_range").unwrap().get::<f64>().unwrap_or(0.05);
+    p.detect_range = rosrust::param("~detect_range").unwrap().get::<f64>().unwrap_or(1.0);
+}
+
 fn main() {
     // Initialize node
     rosrust::init("Marker_listener");
-
+    let mut params = PARAMETERS{low_point_delta_threshold : 0.20,low_point_delta_range : 0.10,x_axis_width_range : 0.05,detect_range : 1.0 };
+    get_parameters(&mut params);
     let mut x0_pub_mutex = Arc::new(Mutex::new(rosrust::publish("/x0points", 100).unwrap()));
-    let marker_sub = rosrust::subscribe("/jsk/marker", 100, move |v: v_msg::visualization_msgs::Marker| {
+
+    let marker_sub = rosrust::subscribe("/jsk/marker", 10, move |v: v_msg::visualization_msgs::Marker| {
 
         let mut x0_pub = x0_pub_mutex.lock().unwrap();
         // Callback for handling received messages
@@ -26,7 +42,7 @@ fn main() {
         let mut min_y = v_msg::geometry_msgs::Point{x:0.0,y:0.0,z:100.0};
         let mut delta_y_point = v_msg::geometry_msgs::Point::default();
         for (cnt,item) in v.points.iter().enumerate(){
-            if item.x.abs() <= 0.05 {
+            if item.x.abs() <= params.x_axis_width_range {
                 let red = v_msg::std_msgs::ColorRGBA{r:1.0,g:0.0,b:0.0,a:1.0,};
                 x0_points.push(v_msg::geometry_msgs::Point::clone(item));
                 // x0_colors.push(v_msg::std_msgs::ColorRGBA::clone(&v.colors[cnt]));
@@ -37,12 +53,13 @@ fn main() {
             }
         }
         for item in &x0_points{
-            if (min_y.y + 0.2) < item.y  && item.y < (min_y.y + 0.3){ //一番下のポイントから大体20〜30cmぐらい上のポイントを捜索
+            if (min_y.y + params.low_point_delta_threshold) < item.y  && item.y < (min_y.y + (params.low_point_delta_threshold+params.low_point_delta_range)){ //一番下のポイントから大体20〜30cmぐらい上のポイントを捜索
                 delta_y_point = v_msg::geometry_msgs::Point::clone(item);
             }
         }
         rosrust::ros_info!("min_y point: {:?},\tdelta_y_point: {:?},\t z delta: {}",min_y.y,delta_y_point.y,min_y.z-delta_y_point.z);
         rosrust::ros_info!("z info = (min_y point: {:?},\tdelta_y_point: {:?})",min_y.z,delta_y_point.z);
+        
         x0_pub_msgs.type_ = 6;
         x0_pub_msgs.points = x0_points;
         x0_pub_msgs.colors = x0_colors;
